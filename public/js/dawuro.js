61590e730d60f10ae16789324fc3ffc959d8abf9
Kwadwo <kwadwon@gmail.com>
2016-06-22 16:54:06 -0400

    initial commit for static page of dawuroapp info page

---
+++ public/js/dawuro.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#to_features").click(function (e) {
+    $("#home").hide();
+    $("#contact").hide();
+    $("#features").show();
+  });
+
+  $("#to_contact").click(function(e) {
+    $("#home").hide();
+    $("#features").hide();
+    $("#contact").show();
+  });
+
+  $("#to_home").click(function(e) {
+    $("#features").hide();
+    $("#contact").hide();
+    $("#home").show();
+  })
+})
